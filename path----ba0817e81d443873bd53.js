9fc36d510034e15834e8dc0e19b2e3f9a2968743
Adam Bulmer <mintuz1990@gmail.com>
2018-04-08 17:42:40 +0100

    Updates

--- path----ba0817e81d443873bd53.js
+++ path----ba0817e81d443873bd53.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{165:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Mintuz"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{138:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Mintuz"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----ba0817e81d443873bd53.js.map
