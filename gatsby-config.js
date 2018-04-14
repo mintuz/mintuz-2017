4f94a274adc3fc11caf5185132cd2feb8278be10
Adam Bulmer <mintuz1990@gmail.com>
2018-04-14 19:14:30 +0100

    Rename pages to posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,8 +15,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/pages`,
-        name: "pages"
+        path: `${__dirname}/src/posts`,
+        name: "posts"
       }
     },
     {
